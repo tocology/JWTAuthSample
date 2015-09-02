154285449bcd85b27ecbf319be502b9848ca9446
tocology <hwangjun7777@gmail.com>
2015-09-02 20:56:01 +0900

    first commit

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  'secret': 'ilovescotchyscotch',
+  'database': "mongodb://JWT:JWT@apollo.modulusmongo.net:27017/pib9Aguz"
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# JWTAuthSample Project
+
+## Reference by 'https://scotch.io/tutorials/authenticate-a-node-js-api-with-json-web-tokens'
